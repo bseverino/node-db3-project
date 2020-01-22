9fc51c2ef74aaa087e05d06570e7a255196a54b3
B Severino <biancasev@gmail.com>
2020-01-22 15:57:14 -0500

    Set up config file with knex, created find and findById methods

---
+++ schemes/scheme-model.js
@@ -0,0 +1,16 @@
+const db = require('../data/db-config.js')
+
+module.exports = {
+    find,
+    findById
+}
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes')
+        .where('id', id)
+        .first()
+}
